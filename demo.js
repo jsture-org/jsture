e69f24726cd162e7a17a4b7af592c64e98b61e40
Atercatus <myleelee2053@naver.com>
2021-12-28 02:20:20 +0900

    feat: 프로젝트 구조 새롭게 설정

--- demo.js
+++
@@ -1,5 +0,0 @@
-// const iscroll = new Iscroll(element, config);
-// const draggable = new Draggable(element, config);
-
-// data attribute.
-//
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { Initializer } from './core/initializer/initializer';
-
-new Initializer().init();
